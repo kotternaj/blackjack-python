d680e3e945981329a3ef95e829b981fc5e60c9ac
Untitled <john.d.reddock@gmail.com>
2018-04-08 12:58:50 -0400

    created total function

--- blackjack.py
+++ blackjack.py
@@ -6,17 +6,17 @@ deck = [2,3,4,5,6,7,8,9,10,11,12,13,14]*4
 def deal(deck):
     hand = []
     for i in range(2):
-        random.shuffl(deck)
+        random.shuffle(deck)
         card = deck.pop()
         if card == 11: card = 'J'
         if card == 12: card = 'Q'
-        if card == 12: card = 'K'
-        if card == 12: card = 'A'
+        if card == 13: card = 'K'
+        if card == 14: card = 'A'
         hand.append(card)
     return hand
 
 def play_again():
-    answer = raw_input('Do you want to play again? y/n: ')
+    answer = raw_input('Do you want to play again? y/n: ').lower()
     if answer == 'y':
         dealers_hand = []
         players_hand = []
@@ -27,8 +27,18 @@ def play_again():
         exit()
 
 def total(hand):
+    total = 0
+    for card in hand:
+        if card == 'J' or card == 'K' or card == 'Q':
+            total += 10
+        elif:
+            if card == 'A':
+                if total >= 11 : total += 1
+            else: total += 11
+        else: total += card
+    return total
 
-def hit(hand):
+# def hit(hand):
 
 def clear():
     if os.name == 'nt':
@@ -36,11 +46,20 @@ def clear():
     if os.name == 'posix':
         os.system('clear')
 
-def show_results():
+# def show_results():
 
-def blackjack(dealers_hand, players_hand):
+# def blackjack(dealers_hand, players_hand):
 
-def score(dealers_hand, players_hand):
+# def score(dealers_hand, players_hand):
 
 def game():
+    print "Welcome to Python Blackjack"
+    players_hand = deal(deck)
+    dealers_hand = deal(deck)
+    
+    print 'Players Hand: %s    Dealers Hand: %s' %(players_hand, dealers_hand)  
+ 
 
+
+if __name__ == "__main__":
+   game()
